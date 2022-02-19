4dd297a3970a5a68d36ec3e6dd168acbb096bb9c
Toby <mrtobez08@gmail.com>
2022-02-19 11:34:10 +0000

    deployment fix attempt no 1

--- next.config.js
+++ next.config.js
@@ -3,11 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-//module.exports = nextConfig
-
-module.exports = {
-  webpack: (config) => {
-    config.experiments = { topLevelAwait: true };
-    return config;
-  },
-};
+module.exports = nextConfig
